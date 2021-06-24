7dc4a151606812c977f5699122af97ea2f02124d
Jifeng Jiang <jjian062@uottawa.ca>
2021-06-24 00:35:04 -0400

    feat: Added basic node server

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # react-ssr
 Server side render with React
+
+1. Added basic node server
---
+++ server/app.js
@@ -0,0 +1,20 @@
+var express = require('express')
+var app = express()
+
+app.get('/', function (req, res) {
+    res.send(
+        `
+        <html>
+            <head>
+                <title>hello</title>
+            </head>
+            <body>
+                <h1>Yo</h1>
+                <p>hello world</p>
+            </body>
+        </html>
+        `
+    )
+})
+
+var server = app.listen(3000)
